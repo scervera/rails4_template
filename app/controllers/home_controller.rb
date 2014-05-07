5d4f08c46acf0125b59554297918128251073b38
Steve Cervera <cervera1@mac.com>
2014-05-06 19:58:42 -0700

    added foundation and new home layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  #SMC - This specifies the layout to use: views/layouts/index.html.erb
+  #layout 'home'
+  
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Mdn2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'articles#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
